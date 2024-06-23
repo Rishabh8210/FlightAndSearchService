cc095333b8c6de9c57a5874adaa25acf19e05513
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-06-23 16:52:16 +0530

    Completed basic sequelize setup for create a db

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const { PORT } = require('./config/serverConfig')
+
 const serverSetup = async () => {
+    
+    // Express object
     const app = express()
+    
+    // Middlewares
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extrended : true}));
+    
+    // Server listening
     app.listen(PORT, () => {
         console.log(`Server is runnning at ${PORT}`);
     })
